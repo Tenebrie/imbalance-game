b1a9253d9c74c4212fc0f7ebd9e8238dbf25a81b
Aleksandr Semjonov <kos94ok@gmail.com>
2020-01-21 23:22:09 +0200

    Preparing for Heroku deployment

--- heroku.sh
+++ heroku.sh
@@ -9,10 +9,6 @@ echo "Moving new client files"
 mkdir server/client
 cp -r client/dist/* server/client/
 echo "Committing new files"
-(cd server && git add client && git commit)
+git add server/client && git commit
 echo "Pushing to Heroku"
-(git push heroku master)
-if [ "$SERVER_CHANGED" = true ] ; then
-	echo "Restoring the stash"
-	(cd server && git stash pop)
-fi
+git push heroku master
