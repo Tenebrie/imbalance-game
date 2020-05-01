084ba808f9dc13441a73b9ff814827e3b9861dc4
Aleksandr Semjonov <kos94ok@gmail.com>
2020-05-01 20:19:17 +0300

    Add missing database migrations

---
+++ server/migrations/1588244750219_Create-editor-decks-table.js
@@ -0,0 +1,33 @@
+exports.shorthands = undefined
+
+exports.up = (pgm) => {
+	pgm.createTable('editor_decks', {
+		id: {
+			type: 'varchar(64)',
+			notNull: true,
+			unique: true
+		},
+		playerId: {
+			type: 'varchar(64)',
+			notNull: true
+		},
+		name: {
+			type: 'varchar(255)',
+			notNull: true
+		},
+		cards: {
+			type: 'jsonb',
+			notNull: true
+		},
+		createdAt: {
+			type: 'timestamp',
+			notNull: true,
+			default: pgm.func('current_timestamp')
+		}
+	})
+	pgm.createIndex('editor_decks', 'id')
+}
+
+exports.down = (pgm) => {
+	pgm.dropTable('editor_decks')
+}
