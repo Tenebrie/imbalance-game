5ff783f19d68d3bfb403f591ed93e05c9fca7c7a
Aleksandr Semjonov <kos94ok@gmail.com>
2020-07-11 01:25:02 +0300

    Fix deck importing

--- client/src/Vue/components/editor/SharedDeckImporter.vue
+++ client/src/Vue/components/editor/SharedDeckImporter.vue
@@ -7,6 +7,7 @@
 <script lang="ts">
 import Vue from 'vue'
 import axios from 'axios'
+import router from '@/Vue/router'
 import PopulatedEditorDeck from '@/utils/editor/PopulatedEditorDeck'
 import Notifications from '@/utils/Notifications'
 import {onMounted} from '@vue/composition-api'
@@ -17,17 +18,18 @@ function Setup() {
 			await importDeck()
 		} catch (e) {
 			Notifications.error('Unable to import deck!')
+			console.error('Unable to import deck', e)
 		}
 	})
 
 	const importDeck = async (): Promise<void> => {
 		const response = await axios.post('/api/decks', {
-			sharedCode: this.$route.params.id
+			sharedCode: router.currentRoute.params.id
 		})
 
 		const deck = response.data.deck as PopulatedEditorDeck
 
-		await this.$router.push({
+		await router.push({
 			name: 'single-deck',
 			params: {
 				id: deck.id
