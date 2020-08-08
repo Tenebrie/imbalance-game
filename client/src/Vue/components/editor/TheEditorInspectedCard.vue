4e659e702ba5b5dc976cc9d78257e37e206d4cc7
Aleksandr Semjonov <kos94ok@gmail.com>
2020-08-09 00:43:37 +0300

    Convert all emails to lowercase

--- client/src/Vue/components/editor/TheEditorInspectedCard.vue
+++ client/src/Vue/components/editor/TheEditorInspectedCard.vue
@@ -48,16 +48,16 @@ export default Vue.extend({
 		padding: 0 16px;
 		pointer-events: none;
 		display: flex;
-		align-items: start;
-		justify-content: start;
+		align-items: flex-start;
+		justify-content: flex-start;
 
 		.overlay-container {
 			position: absolute;
 			width: 100%;
 			height: 100%;
 			display: flex;
-			align-items: start;
-			justify-content: start;
+			align-items: flex-start;
+			justify-content: flex-start;
 		}
 	}
 </style>
---
+++ server/migrations/1596921617861_Migrate-all-usernames-to-lowercase.js
@@ -0,0 +1,11 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined
+
+exports.up = (pgm) => {
+	pgm.db.query('UPDATE players SET email=lower(email)')
+}
+
+exports.down = () => {
+
+}
