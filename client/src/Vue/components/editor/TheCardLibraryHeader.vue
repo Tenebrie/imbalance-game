18bb180dd2030027547e483615333e5aa64316b2
Aleksandr Semjonov <kos94ok@gmail.com>
2020-09-06 19:45:22 +0300

    Deck editor responsive mode

--- client/src/Vue/components/editor/TheCardLibraryHeader.vue
+++ client/src/Vue/components/editor/TheCardLibraryHeader.vue
@@ -1,27 +1,29 @@
 <template>
-	<div class="the-card-library-header">
-		<div class="filters">
-			<button
-				v-for="data in this.factionData"
-				:key="`faction-button-${data.faction}`"
-				class="primary"
-				:class="{ selected: data.faction === selectedFaction }"
-				@click="() => toggleFaction(data.faction)"
-			>
-				{{ data.text }}
-			</button>
+	<div class="the-card-library-header" :class="filtersHiddenClass">
+		<div class="filters" v-if="filtersVisible">
+			<div class="filter-buttons">
+				<button
+					v-for="data in this.factionData"
+					:key="`faction-button-${data.faction}`"
+					class="primary"
+					:class="{ selected: data.faction === selectedFaction }"
+					@click="() => toggleFaction(data.faction)"
+				>
+					{{ data.text }}
+				</button>
+			</div>
 			<inline-tooltip class="tooltip">{{ $locale.get('ui.editor.header.factions.tooltip') }}</inline-tooltip>
-			<div class="separator" />
-			<button
-				v-for="data in this.colorData"
-				:key="`color-button-${data.color}`"
-				class="primary"
-				:class="{ selected: data.color === selectedColor }"
-				@click="() => toggleColor(data.color)"
-			>
-				{{ data.text }}
-			</button>
-			<div class="separator" />
+			<div class="filter-buttons">
+				<button
+					v-for="data in this.colorData"
+					:key="`color-button-${data.color}`"
+					class="primary"
+					:class="{ selected: data.color === selectedColor }"
+					@click="() => toggleColor(data.color)"
+				>
+					{{ data.text }}
+				</button>
+			</div>
 			<div class="checkbox-container">
 				<div class="checkbox">
 					<input id="bigcheckbox" type="checkbox" value="Test" v-model="experimentalToggle" />
@@ -29,11 +31,24 @@
 				</div>
 			</div>
 		</div>
-		<div class="search">
-			<div class="search-input-container">
-				<input type="text" :placeholder="this.$locale.get('ui.editor.library.search.placeholder')" v-model="searchQuery" />
+		<div class="right-side">
+			<div class="search">
+				<div class="search-input-container">
+					<input type="text" :placeholder="this.$locale.get('ui.editor.library.search.placeholder')" v-model="searchQuery" />
+				</div>
+				<button class="secondary" @click="this.clearSearch"><i class="fas fa-times"></i></button>
 			</div>
-			<button class="secondary" @click="this.clearSearch"><i class="fas fa-times"></i></button>
+			<div class="hide-filters-normal">
+				<button class="primary" @click="() => toggleFilters()">
+					<i v-if="!filtersVisible" class="fas fa-eye"></i>
+					<i v-if="filtersVisible" class="fas fa-eye-slash"></i>
+				</button>
+			</div>
+			<div class="hide-filters-mobile">
+				<button class="primary" @click="() => toggleFilters()">
+					Toggle filters
+				</button>
+			</div>
 		</div>
 	</div>
 </template>
@@ -42,7 +57,7 @@
 import store from '@/Vue/store'
 import CardFaction from '@shared/enums/CardFaction'
 import CardColor from '@shared/enums/CardColor'
-import {computed, defineComponent} from '@vue/composition-api'
+import {computed, defineComponent, ref} from '@vue/composition-api'
 import {debounce} from 'throttle-debounce'
 import InlineTooltip from '@/Vue/components/InlineTooltip.vue'
 import {useDecksRouteQuery} from '@/Vue/components/editor/EditorRouteParams'
@@ -71,6 +86,15 @@ export default defineComponent({
 				routeQuery.value.faction = faction
 			}
 		}
+
+		const filtersVisible = ref<boolean>(true)
+		const toggleFilters = () => {
+			filtersVisible.value = !filtersVisible.value
+		}
+
+		const filtersHiddenClass = computed<Record<string, boolean>>(() => ({
+			'filters-hidden': !filtersVisible.value
+		}))
 
 		const colorData = [
 			{ text: 'All', color: null },
@@ -115,12 +139,15 @@ export default defineComponent({
 		})
 
 		return {
+			filtersVisible,
+			filtersHiddenClass,
 			factionData,
 			colorData,
 			selectedFaction,
 			selectedColor,
 			toggleFaction,
 			toggleColor,
+			toggleFilters,
 			clearSearch,
 			experimentalToggle,
 			searchQuery
@@ -132,13 +159,19 @@ export default defineComponent({
 <style scoped lang="scss">
 	@import "../../styles/generic";
 
+	$MOBILE_MODE_THRESHOLD: 1240px;
+	$PLEB_MODE_THRESHOLD: 2050px;
+
 	.the-card-library-header {
 		display: flex;
 		flex-wrap: wrap;
-
-		justify-content: space-between;
-		padding: 8px;
+		padding: 0 8px;
 		height: calc(100% - 16px);
+		justify-content: space-between;
+
+		&.filters-hidden {
+			justify-content: flex-end;
+		}
 
 		button.selected {
 			color: black;
@@ -156,13 +189,19 @@ export default defineComponent({
 
 		.filters {
 			display: flex;
+			flex-wrap: wrap;
+			justify-content: flex-start;
 
 			& > * {
-				flex-grow: 0;
 			}
 
 			.tooltip {
 				font-size: 1.2em;
+			}
+
+			.filter-buttons {
+				display: flex;
+				margin: 8px 0;
 			}
 
 			.separator {
@@ -175,45 +214,106 @@ export default defineComponent({
 
 			button {
 				margin: 0 4px;
+				min-width: 110px;
+
+				&:nth-child(1) {
+					min-width: 50px;
+				}
 			}
 
 			.checkbox-container {
 				display: flex;
 				align-items: center;
 				justify-content: center;
+				margin: 8px 0;
 
 				.checkbox {
 					display: flex;
 				}
 			}
+
+			@media (max-width: $MOBILE_MODE_THRESHOLD) {
+				flex-direction: row;
+				width: 100%;
+				justify-content: center;
+				margin-top: 8px;
+				.filter-buttons {
+					flex-direction: column;
+					margin: 0 8px;
+					width: 100%;
+					button {
+						margin: 4px 0;
+					}
+				}
+			}
 		}
 
-		.search {
-			position: relative;
+		.right-side {
 			display: flex;
-			min-height: 40px;
+			@media (max-width: $PLEB_MODE_THRESHOLD) {
+				width: 100%;
+			}
 
-			.search-input-container {
-				flex: 5;
-				height: 100%;
-				display: flex;
+			@media (max-width: $MOBILE_MODE_THRESHOLD) {
+				width: 100%;
 				flex-direction: column;
-				justify-content: center;
+			}
 
-				input {
-					margin: 0;
-					border: 1px solid darken($COLOR-TEXT, 10);
+			.search {
+				position: relative;
+				display: flex;
+				min-height: 40px;
+				margin: 8px 0;
+				@media (max-width: $PLEB_MODE_THRESHOLD) {
+					width: 100%;
+				}
+
+				.search-input-container {
+					flex: 5;
 					height: 100%;
-					border-radius: 0.25em 0 0 0.25em;
-					border-right: none;
+					display: flex;
+					flex-direction: column;
+					justify-content: center;
+
+					input {
+						margin: 0;
+						border: 1px solid darken($COLOR-TEXT, 10);
+						height: 100%;
+						border-radius: 0.25em 0 0 0.25em;
+						border-right: none;
+					}
+				}
+
+				button {
+					width: 40px;
+					margin: 0;
+					padding: 0;
+					border-radius: 0 0.25em 0.25em 0;
+				}
+
+				@media (max-width: $MOBILE_MODE_THRESHOLD) {
+					width: 100%;
 				}
 			}
 
-			button {
-				flex: 1;
-				margin: 0;
-				padding: 0;
-				border-radius: 0 0.25em 0.25em 0;
+			.hide-filters-normal {
+				margin-top: 4px;
+				margin-left: 8px;
+				button {
+					height: calc(100% - 12px);
+				}
+				@media (max-width: $MOBILE_MODE_THRESHOLD) {
+					display: none;
+				}
+			}
+
+			.hide-filters-mobile {
+				display: none;
+				width: 100%;
+				margin: 8px 0;
+				@media (max-width: $MOBILE_MODE_THRESHOLD) {
+					display: block;
+				}
 			}
 		}
 	}
