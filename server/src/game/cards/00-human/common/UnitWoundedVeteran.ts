add1453130498df9f99c0e7fb6aca737bff4e707
Aleksandr Semjonov <kos94ok@gmail.com>
2020-09-21 01:36:36 +0300

    Add Wounded Veteran

---
+++ server/src/game/cards/00-human/common/UnitWoundedVeteran.ts
@@ -0,0 +1,39 @@
+import CardType from '@shared/enums/CardType'
+import ServerCard from '../../../models/ServerCard'
+import ServerGame from '../../../models/ServerGame'
+import CardColor from '@shared/enums/CardColor'
+import CardTribe from '@shared/enums/CardTribe'
+import CardFaction from '@shared/enums/CardFaction'
+import GameEventType from '@shared/enums/GameEventType'
+import CardFeature from '@shared/enums/CardFeature'
+import BuffStrength from '../../../buffs/BuffStrength'
+import ExpansionSet from '@shared/enums/ExpansionSet'
+import ServerDamageInstance from '../../../models/ServerDamageSource'
+
+export default class UnitWoundedVeteran extends ServerCard {
+	damageToSelf = 10
+
+	constructor(game: ServerGame) {
+		super(game, {
+			type: CardType.UNIT,
+			color: CardColor.BRONZE,
+			faction: CardFaction.HUMAN,
+			tribes: [CardTribe.HUMAN],
+			features: [CardFeature.KEYWORD_DEPLOY],
+			stats: {
+				power: 20,
+			},
+			expansionSet: ExpansionSet.BASE,
+		})
+		this.dynamicTextVariables = {
+			damageToSelf: this.damageToSelf
+		}
+
+		this.createEffect(GameEventType.UNIT_DEPLOYED)
+			.perform(() => this.onDeploy())
+	}
+
+	private onDeploy(): void {
+		this.dealDamage(ServerDamageInstance.fromCard(this.damageToSelf, this))
+	}
+}
