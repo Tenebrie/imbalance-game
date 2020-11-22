bc68d9c29668eefb9b35bf9669b1503d196cf7ef
Vadim Prokopev <vadimjprokopev@gmail.com>
2020-11-22 19:52:20 +0200

    Add Illusion Twin epic card

---
+++ server/src/game/cards/01-arcane/epic/HeroIllusionTwin.ts
@@ -0,0 +1,42 @@
+import CardType from '@shared/enums/CardType'
+import CardColor from '@shared/enums/CardColor'
+import ServerCard from '../../../models/ServerCard'
+import ServerGame from '../../../models/ServerGame'
+import ServerDamageInstance from '../../../models/ServerDamageSource'
+import BuffStrength from '../../../buffs/BuffStrength'
+import CardFaction from '@shared/enums/CardFaction'
+import ExpansionSet from '@shared/enums/ExpansionSet'
+import GameEventType from '@shared/enums/GameEventType'
+import CardLocation from '@shared/enums/CardLocation'
+
+export default class HeroIllusionTwin extends ServerCard {
+    powerGiven = 3
+    powerLost = 1
+
+	constructor(game: ServerGame) {
+		super(game, {
+			type: CardType.UNIT,
+			color: CardColor.SILVER,
+			faction: CardFaction.ARCANE,
+			stats: {
+				power: 9,
+			},
+			expansionSet: ExpansionSet.BASE,
+		})
+		this.dynamicTextVariables = {
+			powerGiven: this.powerGiven,
+			powerLost: this.powerLost
+		}
+
+		this.createCallback(GameEventType.TURN_ENDED, [CardLocation.BOARD])
+            .require(({ player }) => player === this.owner)
+            .perform(({ player }) => {
+                const copyInHand = player.cardHand.unitCards.find(u => u.class === this.class)
+                if (copyInHand) {
+                    this.dealDamage(ServerDamageInstance.fromCard(this.powerLost, this));
+                    copyInHand.buffs.addMultiple(BuffStrength, this.powerGiven, this, Infinity);
+                }
+            })
+
+	}
+}
