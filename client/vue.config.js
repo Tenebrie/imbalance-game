a756c96ababb44c8033d8ba5d1a3841a40558c5c
Aleksandr Semjonov <kos94ok@gmail.com>
2021-01-07 22:14:40 +0200

    Fix client logging

--- client/vue.config.js
+++ client/vue.config.js
@@ -8,12 +8,9 @@ module.exports = {
 			},
 		},
 	},
-	chainWebpack: (config) => {
-		// remove vue-cli-service's progress output
-		config.plugins.delete('progress')
-	},
 	devServer: {
 		publicPath: '/',
+		progress: false,
 		proxy: {
 			'/api': {
 				// logLevel: 'debug',
