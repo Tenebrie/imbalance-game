bf4ab9b62fab07ff35176789433033a73d57bbcf
Vadim Prokopev <vadimjprokopev@gmail.com>
2021-05-29 16:43:11 +0300

    Add multi-color and multi-faction select

--- client/src/Vue/components/editor/EditorRouteQuery.ts
+++ client/src/Vue/components/editor/EditorRouteQuery.ts
@@ -14,33 +14,34 @@ router.afterEach((route) => {
 })
 
 interface Query {
-	color: CardColor | null
-	faction: CardFaction | null
+	color: Array<CardColor> | null
+	faction: Array<CardFaction> | null
 	experimental: boolean
 }
 
 export const useDecksRouteQuery = (): ComputedRef => {
 	return computed(
 		(): Query => ({
-			get color(): CardColor | null {
+			get color(): Array<CardColor> | null {
 				if (routeData.value.query['color'] === undefined) {
 					return null
 				}
-				return Number(routeData.value.query['color'])
+
+				return routeData.value.query['color'].split(',').map((c: string) => Number(c))
 			},
-			set color(value: CardColor | null) {
-				const color = value === null ? undefined : String(value)
+			set color(value: Array<CardColor> | null) {
+				const color = value === null ? undefined : value.join(',')
 				router.push({ query: { ...router.currentRoute.value.query, color: color } })
 			},
 
-			get faction(): CardFaction | null {
+			get faction(): Array<CardFaction> | null {
 				if (routeData.value.query['faction'] === undefined) {
 					return null
 				}
-				return Number(routeData.value.query['faction'])
+				return routeData.value.query['faction'].split(',').map((f: string) => Number(f))
 			},
-			set faction(value: CardFaction | null) {
-				const faction = value === null ? undefined : String(value)
+			set faction(value: Array<CardFaction> | null) {
+				const faction = value === null ? undefined : value.join(',')
 				router.push({ query: { ...router.currentRoute.value.query, faction: faction } })
 			},
 
