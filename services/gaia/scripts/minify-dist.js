c4805351659dd10b429cca31714e6e43b442ef0c
Aleksandr Semjonov <kos94ok@gmail.com>
2021-09-19 04:52:53 +0300

    Add Gaia code minification

---
+++ services/gaia/scripts/minify-dist.js
@@ -0,0 +1,37 @@
+const fs = require('fs')
+const glob = require('glob')
+const sharp = require('sharp')
+const uglify = require('uglify-js')
+
+const AsciiColor = {
+	BLACK: '\u001b[30;1m',
+	RED: '\u001b[31;1m',
+	GREEN: '\u001b[32;1m',
+	YELLOW: '\u001b[33;1m',
+	BLUE: '\u001b[34;1m',
+	MAGENTA: '\u001b[35;1m',
+	CYAN: '\u001b[36;1m',
+	WHITE: '\u001b[37;1m',
+}
+
+const colorize = (text, color) => {
+	return `${color}${text}\u001b[0m`
+}
+
+glob('dist/**/*.@(js)', async (er, files) => {
+	const fileCode = await Promise.all(
+		files.map(
+			(file) =>
+				new Promise((resolve) => {
+					resolve({
+						file,
+						...uglify.minify(fs.readFileSync(file, 'utf8')),
+					})
+				})
+		)
+	)
+
+	await Promise.all(fileCode.map((file) => fs.writeFileSync(file.file, file.code)))
+
+	console.log(`Successfully minified ${fileCode.length} .js files.`)
+})
