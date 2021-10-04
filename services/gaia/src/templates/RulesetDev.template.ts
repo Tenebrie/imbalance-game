9aa2be6ec1fa636a2bf693847047ad8833abdc7b
Aleksandr Semjonov <kos94ok@gmail.com>
2021-10-05 00:59:17 +0300

    Add dev ruleset template message

--- services/gaia/src/templates/RulesetDev.template.ts
+++ services/gaia/src/templates/RulesetDev.template.ts
@@ -8,6 +8,14 @@ import UnitChallengeDummyVanillaWarrior from '@src/game/cards/10-challenge/ai-00
 import { ServerRuleset } from '@src/game/models/rulesets/ServerRuleset'
 import ServerGame from '@src/game/models/ServerGame'
 
+/**
+ * This is the template file for the development ruleset definition.
+ *
+ * When Gaia is started (i.e. when the development environment is run), a copy of this file
+ * will be created as your personal development ruleset, if it doesn't exist already.
+ * Before editing, make sure you're working on `src/game/rulesets/RulesetDev.ts` file, and not
+ * on the `RulesetDev.template.ts` file.
+ */
 export default class RulesetDev extends ServerRuleset {
 	constructor(game: ServerGame) {
 		super(game, {
